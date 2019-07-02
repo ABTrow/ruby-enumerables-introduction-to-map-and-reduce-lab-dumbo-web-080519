3753694786fbbea49da7fc0411a8f14f08dfb5c0
Austin Trow <austintrow@gmail.com>
2019-07-02 00:19:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,3 +5,19 @@ def map_to_negativize(source_array)
   
   source_array
 end
+
+def map_to_double(source_array)
+  source_array.length.times do |index|
+    source_array[index] *= 2
+  end
+  
+  source_array
+end
+
+def map_to_negativize(source_array)
+  source_array.length.times do |index|
+    source_array[index] *= -1
+  end
+  
+  source_array
+end
