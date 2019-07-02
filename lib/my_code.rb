37c8b4877a3c3ec6e923a4e699ca40ffcd8e14d8
Austin Trow <austintrow@gmail.com>
2019-07-02 00:19:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,10 +14,11 @@ def map_to_double(source_array)
   source_array
 end
 
-def map_to_negativize(source_array)
+def map_to_square(source_array)
   source_array.length.times do |index|
-    source_array[index] *= -1
+    source_array[index] = source_array[index] ** 2
   end
   
   source_array
 end
+
